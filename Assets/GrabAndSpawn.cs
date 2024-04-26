81eaad6234c7cfeb36976cef3e7dc0d2b340e1f3
MarzLars <lars_gustav@hotmail.com>
2024-04-26 02:02:08 +0200

    Spawners can now be triggered

---
+++ Assets/GrabAndSpawn.cs
@@ -0,0 +1,30 @@
+using Obstacles;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabAndSpawn : MonoBehaviour
+{
+    private XRGrabInteractable _grabInteractable;
+    private FallingObstacleFactoryWrapper _fallingObstacleFactoryWrapper;
+
+    private void Awake()
+    {
+        _grabInteractable = GetComponent<XRGrabInteractable>();
+        _fallingObstacleFactoryWrapper = GetComponent<FallingObstacleFactoryWrapper>();
+    }
+
+    private void OnEnable()
+    {
+        _grabInteractable.selectEntered.AddListener(HandleSelectEntered);
+    }
+
+    private void OnDisable()
+    {
+        _grabInteractable.selectEntered.RemoveListener(HandleSelectEntered);
+    }
+
+    private void HandleSelectEntered(SelectEnterEventArgs args)
+    {
+        _fallingObstacleFactoryWrapper.SpawnObstacle();
+    }
+}
